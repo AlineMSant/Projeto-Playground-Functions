5a7d9c08d33679686c786fa9a6dde3519601b4b3
AlineMSant <alinems4120@gmail.com>
2022-10-28 00:16:15 -0300

    requisito 11 falta retornar erro quando repete numeros 3 vezes ou mais

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,19 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let retorno;
+
+if(array.length > 11 || array.length < 11){
+  retorno = 'Array com tamanho incorreto.';
+}else{
+  retorno = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
+}
+
+for(i=0; i<array.length; i=i+1){
+  if(array[i] < 0 || array[i] > 9){
+    retorno = 'não é possível gerar um número de telefone com esses valores';
+  }
+}
+return retorno;
 }
 
 // Desafio 12
@@ -13,9 +26,6 @@ function hydrate() {
   // seu código aqui
 }
 
-
-let index = 20;
-console.log(index);
 
 module.exports = {
   generatePhoneNumber,
