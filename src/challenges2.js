009a060dff4d4f47438f016b9ec05370841f1dbe
AlineMSant <alinems4120@gmail.com>
2022-10-29 16:14:15 -0300

    finaliza exercicio 11 bonus

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,15 @@
 // Desafio 11
 function generatePhoneNumber(array) {
   let retorno;
+  let somaUm = 0;
+  let somaDois = 0;
+  let somaTres = 0;
+  let somaQuatro = 0;
+  let somaCinco = 0;
+  let somaSeis = 0;
+  let somaSete = 0;
+  let somaOito = 0;
+  let somaNove = 0;
 
 if(array.length > 11 || array.length < 11){
   retorno = 'Array com tamanho incorreto.';
@@ -12,9 +21,45 @@ for(i=0; i<array.length; i=i+1){
   if(array[i] < 0 || array[i] > 9){
     retorno = 'não é possível gerar um número de telefone com esses valores';
   }
+  if(array[i] < 0 || array[i] > 9 && array.length > 11){
+  retorno = 'Array com tamanho incorreto.';
+  }
+  if(array[i] < 0 || array[i] > 9 && array.length < 11){
+    retorno = 'não é possível gerar um número de telefone com esses valores';
+  }
 }
+
+
+for(i=0; i<array.length; i=i+1){
+  if(array[i] === 1){
+    somaUm = somaUm + 1;
+  }else if(array[i] === 2){
+    somaDois = somaDois + 1;
+  }else if(array[i] === 3){
+    somaTres = somaTres + 1;
+  }else if(array[i] === 4){
+    somaQuatro = somaQuatro + 1;
+  }else if(array[i] === 5){
+    somaCinco = somaCinco + 1;
+  }else if(array[i] === 6){
+    somaSeis = somaSeis + 1;
+  }else if(array[i] === 7){
+    somaSete = somaSete + 1;
+  }else if(array[i] === 8){
+    somaOito = somaOito + 1;
+  }else if(array[i] === 9){
+    somaNove = somaNove + 1;
+  }
+}
+
+if(somaUm >=3 || somaDois >= 3 || somaTres >= 3 || somaQuatro >= 3 || somaCinco >= 3 || somaSeis >= 3 || somaSete >=3 || somaOito>=3 || somaNove >= 3){
+  retorno = 'não é possível gerar um número de telefone com esses valores';
+}
+
 return retorno;
 }
+
+console.log(generatePhoneNumber([0,2,3,4,4,2,7,8,9,9,4]));
 
 // Desafio 12
 function triangleCheck(lineA,lineB,lineC) {
